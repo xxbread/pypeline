327a047ee76236f1156eee2d64094da567977427
xxBread <dimitrijkress4@gmail.com>
2026-02-22 11:13:54 +0100

    util update
    adding a custom version of shutil.rmtree

--- Pypeline/util/tmp.py
+++ Pypeline/util/tmp.py
@@ -1,10 +1,46 @@
 
 import os
+import stat
+
+class Util:
+
+    @staticmethod
+    def rmReadonly(path: os.PathLike) -> None:
+        try:
+            os.chmod(path, stat.S_IWRITE)
+        except:
+            pass
+
+    @staticmethod
+    def rmTree(path: os.PathLike, raiseError: bool = False) -> None:
+        
+        if not os.path.exists(path):
+            return
+        
+        if os.path.isfile(path) or os.path.islink(path):
+            Util.rmReadonly(path)
+            try:
+                os.remove(path)
+            except Exception as e:
+                if raiseError:
+                    raise e
+
+        elif os.path.isdir(path):
+            for entry in os.listdir(path):
+                Util.rmTree(os.path.join(path, entry), raiseError)
+            
+            Util.rmReadonly(path)
+            try:
+                os.rmdir(path)
+            except Exception as e:
+                if raiseError:
+                    raise e
 
 class Tmp:
     path_file = os.path.realpath(__file__)
     path_dir = os.path.dirname(path_file)
     root = os.path.join(os.path.dirname(path_dir), "_temp")
+    Util.rmTree(root, raiseError=True)
     os.makedirs(root, exist_ok=True)
 
     @classmethod
